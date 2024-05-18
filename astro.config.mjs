7c86321d7b5750378b8d6f2d8eb393d75a788b0f
James D <james@integratn.io>
2024-05-18 12:58:20 -0600

    lots of cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
+import netlify from '@astrojs/netlify/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://integratn.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  adapter: netlify(),
 });

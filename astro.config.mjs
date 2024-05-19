ba72c213d062722d80a50ad027d97b507df7e46c
James D <james@integratn.io>
2024-05-18 18:35:48 -0600

    add robots generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,17 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 import netlify from '@astrojs/netlify/static';
+import robotsTxt from 'astro-robots-txt';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://integratn.io',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    robotsTxt()
+  ],
   adapter: netlify(),
 });
